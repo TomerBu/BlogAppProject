e351f912566dd38cca81f139ed20d883645e9fb0
BuTomer <bu.tomer@gmail.com>
2023-06-15 17:55:20 +0300

    removed imports

--- src/main/java/edu/tomerbu/blogproject/security/BlogSecurityConfig.java
+++ src/main/java/edu/tomerbu/blogproject/security/BlogSecurityConfig.java
@@ -7,14 +7,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -36,30 +30,8 @@ public class BlogSecurityConfig {
                 .build();
     }
 
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        //in memory users:
-//        UserDetails user1 = User.builder()
-//                .username("user")
-//                .password(passwordEncoder().encode("password"))
-//                .roles("USER").build();
-//
-//        UserDetails user2 = User.builder()
-//                .username("admin")
-//                .password(passwordEncoder().encode("admin"))
-//                .roles("ADMIN").build();
-//
-//
-//
-//
-//        return new InMemoryUserDetailsManager(
-//                user1, user2
-//        );
-//    }
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-        //return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
