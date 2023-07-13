e7ba3045ff24765558b7bb3447b771b376132727
BuTomer <bu.tomer@gmail.com>
2023-07-13 21:43:43 +0300

    initial database seed

--- src/main/java/edu/tomerbu/blogproject/BlogProjectApplication.java
+++ src/main/java/edu/tomerbu/blogproject/BlogProjectApplication.java
@@ -1,14 +1,22 @@
 package edu.tomerbu.blogproject;
 
 import edu.tomerbu.blogproject.config.BlogJWTConfig;
+import edu.tomerbu.blogproject.entity.Role;
 import edu.tomerbu.blogproject.error.BlogException;
+import edu.tomerbu.blogproject.repository.RoleRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 @EnableConfigurationProperties(BlogJWTConfig.class)
-public class BlogProjectApplication {
+public class BlogProjectApplication implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
 
     public static void main(String[] args) {
 
@@ -17,6 +25,17 @@ public class BlogProjectApplication {
             SpringApplication.run(BlogProjectApplication.class, args);
         } catch (BlogException b) {
             //send to log server
+        }
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        //check the size
+        if (roleRepository.findAll().size() == 0) {
+            //add roles to role repo:
+            //insert
+            roleRepository.save(new Role(1L, "ROLE_ADMIN"));
         }
     }
 }
